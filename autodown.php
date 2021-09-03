6ba207fa58565fd67c476a1b6eba0f8df6edc13a
beizishaozi <984363019@qq.com>
2021-09-03 22:50:38 +0800

    Create autodown.php

---
+++ autodown.php
@@ -0,0 +1,30 @@
+<?php 
+/** 
+* 获取HTTP请求原文 
+* @return string 
+*/
+header('Access-Control-Allow-Origin:*');  
+//function get_http_raw() { 
+	$raw = ''; 
+ 
+	// (1) 请求行 
+	$raw .= $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n"; 
+ 
+	// (2) 请求Headers 
+	foreach($_SERVER as $key => $value) { 
+		if(substr($key, 0, 5) === 'HTTP_') { 
+			$key = substr($key, 5); 
+			$key = str_replace('_', '-', $key);  
+			$raw .= $key.': '.$value."\r\n"; 
+		} 
+	} 
+ 
+	// (3) 空行 
+	$raw .= "\r\n"; 
+ 
+	// (4) 请求Body 
+	$raw .= file_get_contents('php://input'); 
+	echo "<script> alert('{$raw}') </script>";
+	//file_put_contents("D:/data.txt",$raw);
+
+?>
