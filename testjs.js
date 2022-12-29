b26309515df5f6e755259b169715c65fa8c9351c
zhangy <984363019@qq.com>
2022-12-29 23:14:01 +0800

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -5,5 +5,10 @@ oDiv.innerHTML = '566';
 document.body.appendChild(oDiv);
 
 
+function myFun(data){
+    var div = document.getElementbyId('div1');
+    div.innerHTML = data;
+}
+
 window.mkAobj.bridgejs("app", "getUserInfo", "{'callback':'oDiv.innerHTML='}");
 
