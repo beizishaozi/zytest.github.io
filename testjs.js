a11c1a2ffb54b4f35b591a17638fe90debc4be22
zhangy <984363019@qq.com>
2022-12-29 20:02:21 +0800

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,14 +1,9 @@
-function getUserInfo(result){
-    
-}
-window.mkAobj.bridgejs("app", "getUserInfo", "{'callback':'var oDiv = document.createElement(\'div\');oDiv.id = \'div1\';document.body.appendChild(oDiv);oDiv.innerHTML = '}");
 
-console.log("this is a test");
-
-function getUser(a){
-    console.log(a);
-}
-
-//var oDiv = document.createElement('div');oDiv.id = 'div1';
+var oDiv = document.createElement('div');
+oDiv.id = 'div1';
 //    oDiv.innerHTML = '566';
-//    document.body.appendChild(oDiv);
+document.body.appendChild(oDiv);
+
+
+window.mkAobj.bridgejs("app", "getUserInfo", "{'callback':'oDiv.innerHTML='}");
+
