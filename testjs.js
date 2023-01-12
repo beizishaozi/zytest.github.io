98b1f4f1d6979fd88acb2aef2b052e3c0f74439c
zhangy <984363019@qq.com>
2023-01-12 23:20:38 +0800

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,14 +1,4 @@
-
-var oDiv = document.createElement('div');
-oDiv.id = 'div1';
-oDiv.innerHTML = '566';
-document.body.appendChild(oDiv);
-
-
 function myFun(data){
-    var div = document.getElementbyId('div1');
-    div.innerHTML = data;
+    document.write(data);
 }
-
-window.mkAobj.bridgejs("app", "getUserInfo", "{'callback':'oDiv.innerHTML='}");
 
